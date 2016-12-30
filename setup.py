03a653162b994e83f5ce5bd4321cc745902cff9c
Tomas Stibrany <tms.stibrany@gmail.com>
2016-12-30 18:37:34 +0100

    Version bump to 0.6.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='anacode',
-    version='0.6',
+    version='0.6.1',
     description='Anacode API querying and aggregation library',
     author='Tomas Stibrany',
     author_email='tomas.stibrany@anacode.de',
     url='https://github.com/anacode/anacode-toolkit',
-    download_url='https://github.com/anacode/anacode-toolkit/tarball/0.6',
+    download_url='https://github.com/anacode/anacode-toolkit/tarball/0.6.1',
     license='BSD-3-Clause',
     keywords=['anacode', 'nlp', 'chinese'],
     packages=find_packages(),
