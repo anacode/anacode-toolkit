ccba37ccb857c63dcb1e1824947ca35ff6e92a88
Tomas Stibrany <tms.stibrany@gmail.com>
2016-12-30 18:25:17 +0100

    Version bump to 0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='anacode',
-    version='0.5',
+    version='0.6',
     description='Anacode API querying and aggregation library',
     author='Tomas Stibrany',
     author_email='tomas.stibrany@anacode.de',
