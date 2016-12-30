2703287eaba20c9943ebf81c0a1bbf4bb6383baa
Tomas Stibrany <tms.stibrany@gmail.com>
2016-12-30 13:42:36 +0100

    Added more metadata to setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name='anacode',
     version='0.5',
-    description='Anacode GmbH aggregation library',
+    description='Anacode GmbH querying and aggregation library',
     author='Tomas Stibrany',
     author_email='tomas.stibrany@anacode.de',
+    url='https://github.com/anacode/anacode-toolkit',
+    license='BSD-3-Clause',
+    keywords='anacode',
     packages=find_packages(),
     install_requires=['requests', 'pandas', 'seaborn', 'matplotlib',
                       'wordcloud', 'pillow'],
