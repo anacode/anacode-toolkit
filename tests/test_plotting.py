cabd9f36d403ff7f8d7ef64368daf83685bd001c
Tomas Stibrany <tms.stibrany@gmail.com>
2017-08-24 11:23:56 +0200

    Added new test case for concept whitespacing

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -205,6 +205,7 @@ def test_correct_title_change(plot_name, series, expected):
     ('Space', 'Space'),
     ('Nimbus2000', 'Nimbus 2000'),
     ('24Bank', '24 Bank'),
+    ('TeslaX', 'Tesla X'),
 ])
 def test_explode_concepts(string, exploded):
     assert plotting.explode_capitalized(string) == exploded
